1ce334ef1274ed72fbe73e182c4bf7277731ef4f
yalh76 <yalh@yahoo.com>
2020-06-05 13:49:38 +0200

    Fix ruby version

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,9 +6,8 @@
 
 # dependencies used by the app
 pkg_dependencies="imagemagick ffmpeg libpq-dev libxml2-dev libxslt1-dev file git-core g++ libprotobuf-dev protobuf-compiler pkg-config gcc autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3|libgdbm6 libgdbm-dev redis-server redis-tools postgresql postgresql-contrib libidn11-dev libicu-dev libjemalloc-dev curl apt-transport-https"
-RUBY_VERSION="2.6.6"
 
-RUBY_VERSION="2.6.5"
+RUBY_VERSION="2.6.6"
 
 BUNDLER_VERSION="1.17.3"
 
