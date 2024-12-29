d3cb68ee5ca50f2d4edb7183a36dcaf47b731df1
yalh76 <yalh@yahoo.com>
2024-12-29 18:29:59 +0100

    fix missing ld_preload

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -8,30 +8,23 @@ memory_needed="2560"
 ruby_version=3.3.5
 nodejs_version=20
 
-# Workaround for Mastodon on Bullseye
-# See https://github.com/mastodon/mastodon/issues/15751#issuecomment-873594463
-if [ "$(lsb_release --codename --short)" = "bullseye" ];
-then
-	case $YNH_ARCH in
-		amd64)
-			ld_preload="LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libjemalloc.so"
-			;;
-		arm64)
-			ld_preload="LD_PRELOAD=/usr/lib/aarch64-linux-gnu/libjemalloc.so"
-			;;
-		armhf)
-			ld_preload="LD_PRELOAD=/usr/lib/arm-linux-gnueabihf/libjemalloc.so"
-			;;
-		armel)
-			ld_preload="LD_PRELOAD=/usr/lib/arm-linux-gnueabi/libjemalloc.so"
-			;;
-		i386)
-			ld_preload="LD_PRELOAD=/usr/lib/i386-linux-gnu/libjemalloc.so"
-			;;
-	esac
-else
-	ld_preload=""
-fi
+case $YNH_ARCH in
+	amd64)
+		ld_preload="LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libjemalloc.so"
+		;;
+	arm64)
+		ld_preload="LD_PRELOAD=/usr/lib/aarch64-linux-gnu/libjemalloc.so"
+		;;
+	armhf)
+		ld_preload="LD_PRELOAD=/usr/lib/arm-linux-gnueabihf/libjemalloc.so"
+		;;
+	armel)
+		ld_preload="LD_PRELOAD=/usr/lib/arm-linux-gnueabi/libjemalloc.so"
+		;;
+	i386)
+		ld_preload="LD_PRELOAD=/usr/lib/i386-linux-gnu/libjemalloc.so"
+		;;
+esac
 
 #=================================================
 # PERSONAL HELPERS
